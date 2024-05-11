e52db68f4cd403be3f0216c85a9561a0cc59ca80
Subasree S <subasree3008@gmail.com>
2024-05-11 12:40:19 +0000

    Sync LeetCode submission Runtime - 724 ms (5.01%), Memory - 0.0B (100.00%)

--- 0175-combine-two-tables/solution.sql
+++ 0175-combine-two-tables/solution.sql
@@ -1,11 +1,2 @@
 # Write your MySQL query statement below
-SELECT
-    p.firstName,
-        p.lastName,
-            a.city,
-                a.state
-                FROM
-                    Person p
-                    LEFT JOIN
-                        Address a ON p.personId = a.personId;
-                        
+SELECT Person.firstName,Person.lastName,Address.city,Address.state from Person left outer join Address on Person.personId=Address.personId; 
