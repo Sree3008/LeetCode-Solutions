dd80f3437ed9de0b187af41405e09dbc2a6ebb1e
Subasree S <subasree3008@gmail.com>
2024-09-04 17:09:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.1 MB (99.97%)

---
+++ 0027-remove-element/solution.java
@@ -0,0 +1,43 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0; // Pointer for the position of the next element not equal to val
+        
+        // Iterate through the array
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        
+        // i is the count of elements not equal to val
+        return i;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+        // Example 1
+        int[] nums1 = {3, 2, 2, 3};
+        int val1 = 3;
+        int k1 = sol.removeElement(nums1, val1);
+        System.out.println("Output: " + k1); // Output: 2
+        System.out.print("Array: ");
+        for (int i = 0; i < k1; i++) {
+            System.out.print(nums1[i] + " ");
+        }
+        System.out.println();
+        
+        // Example 2
+        int[] nums2 = {0, 1, 2, 2, 3, 0, 4, 2};
+        int val2 = 2;
+        int k2 = sol.removeElement(nums2, val2);
+        System.out.println("Output: " + k2); // Output: 5
+        System.out.print("Array: ");
+        for (int i = 0; i < k2; i++) {
+            System.out.print(nums2[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
