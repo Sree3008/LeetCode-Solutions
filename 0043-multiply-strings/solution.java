662837e54343c2afd58f31d97f6998493d7b4be0
Subasree S <subasree3008@gmail.com>
2025-09-06 06:36:09 +0000

    Sync LeetCode submission Runtime - 8 ms (16.54%), Memory - 42.9 MB (99.74%)

---
+++ 0043-multiply-strings/solution.java
@@ -0,0 +1,9 @@
+import java.math.BigInteger;
+class Solution {
+    public String multiply(String num1, String num2) {
+        BigInteger n = new BigInteger(num1);
+        BigInteger m = new BigInteger(num2);
+        BigInteger k = n.multiply(m);
+        return String.valueOf(k);
+    }
+}
