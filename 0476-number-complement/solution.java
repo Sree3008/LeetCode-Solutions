390df14d05dcd67849af9261a2dd13bf41d4cb53
Subasree S <subasree3008@gmail.com>
2025-09-19 05:13:35 +0000

    Sync LeetCode submission Runtime - 2 ms (11.15%), Memory - 41.2 MB (99.95%)

---
+++ 0476-number-complement/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findComplement(int num) {
+        String s=Integer.toBinaryString(num);
+        String s1="";
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='0'){
+                s1=s1+'1';
+            }
+            else{
+                s1=s1+'0';
+            }
+        }
+        return Integer.parseInt(s1,2);
+    }
+}
