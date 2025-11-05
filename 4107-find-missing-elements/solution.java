c235b63ee1c47cd66a0e2e233f82755ea242f704
Subasree S <subasree3008@gmail.com>
2025-11-05 09:45:24 +0000

    Sync LeetCode submission Runtime - 8 ms (11.18%), Memory - 47.1 MB (25.83%)

--- 4107-find-missing-elements/solution.java
+++ 4107-find-missing-elements/solution.java
@@ -1,12 +1,24 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
         Arrays.sort(nums);
-        ArrayList<Integer> list=new ArrayList<>();
-        for(int i=nums[0];i<=nums[nums.length-1];i++){
-            if(Arrays.binarySearch(nums,i)<0){
-            list.add(i);    
-            }
+        // ArrayList<Integer> list=new ArrayList<>();
+        // for(int i=nums[0];i<=nums[nums.length-1];i++){
+        //     if(Arrays.binarySearch(nums,i)<0){
+        //     list.add(i);    
+        //     }
+        // }
+        // return list;
+    HashSet<Integer> list=new HashSet<>();
+    List<Integer> list1=new ArrayList<>();
+    for(int i=0;i<nums.length;i++){
+        list.add(nums[i]);
+    }
+    for(int j=nums[0];j<nums[nums.length-1];j++){
+        if(list.add(j)){
+            list1.add(j);
         }
-        return list;
+    }
+    return list1;
+        
     }
 }
