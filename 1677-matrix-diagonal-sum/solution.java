840148342fd797dcdb9c3eea822410db9de822e4
Subasree S <subasree3008@gmail.com>
2025-11-07 09:55:46 +0000

    Sync LeetCode submission Runtime - 1 ms (33.72%), Memory - 46.5 MB (31.51%)

--- 1677-matrix-diagonal-sum/solution.java
+++ 1677-matrix-diagonal-sum/solution.java
@@ -3,7 +3,7 @@ class Solution {
         int sum=0;
         for(int i=0;i<mat.length;i++){
             for(int j=0;j<mat.length;j++){
-                if(i==j||i+j==mat.length-1){
+                if(i==j||j==mat.length-i-1){
                     sum=sum+mat[i][j];
                 }
             }
