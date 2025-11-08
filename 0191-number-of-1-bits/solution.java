5a7ad0e1ea399bd5fe2c2e18ffc1f654ec5364bd
Subasree S <subasree3008@gmail.com>
2025-11-08 08:04:05 +0000

    Sync LeetCode submission Runtime - 1 ms (30.84%), Memory - 42.2 MB (91.19%)

--- 0191-number-of-1-bits/solution.java
+++ 0191-number-of-1-bits/solution.java
@@ -1,12 +1,11 @@
 class Solution {
     public int hammingWeight(int n) {
-        String str = Integer.toBinaryString(n);
         int c=0;
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
-            if(ch=='1'){
+        while(n!=0){
+            if((n&1)==1){
                 c++;
             }
+            n>>=1;
         }
         return c;
     }
