6b89b3e7a0ce5fdd9326f826b92b60e3eee3c69c
Subasree S <subasree3008@gmail.com>
2025-11-09 02:36:38 +0000

    Sync LeetCode submission Runtime - 7 ms (30.56%), Memory - 44.5 MB (51.13%)

---
+++ 4115-minimum-distance-between-three-equal-elements-i/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumDistance(int[] nums) {
+        int n=nums.length;
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    if(nums[i]==nums[j]&&nums[j]==nums[k]){
+                        int dis=Math.abs(i-j)+Math.abs(j-k)+Math.abs(k-i);
+                        ans=Math.min(ans,dis);
+                    }
+                }
+            }
+        }
+        return ans==Integer.MAX_VALUE?-1:ans;
+    }
+}
