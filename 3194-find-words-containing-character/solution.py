0c19d9b0a5a714587db1ab2b445630b93f416027
Subasree S <subasree3008@gmail.com>
2025-11-12 05:09:03 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

---
+++ 3194-find-words-containing-character/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+       return [ind for ind,val in enumerate(words) if x in val]
+
+            
+
+            
+        
