a41d58c18163dbe8e3f2393c85a78fa2bc254434
Subasree S <subasree3008@gmail.com>
2025-11-23 07:25:04 +0000

    Sync LeetCode submission Runtime - 19 ms (21.09%), Memory - 22.8 MB (33.93%)

---
+++ 0049-group-anagrams/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp = {}
+
+        for s in strs:
+            count = [0] * 26
+            for ch in s:
+                count[ord(ch) - ord('a')] += 1
+
+            key = tuple(count)
+            if key not in mp:
+                mp[key] = []
+            mp[key].append(s)
+
+        return list(mp.values())
+
