db587a58cc090e2010d4915e09232df4393c66db
Subasree S <subasree3008@gmail.com>
2025-11-27 16:16:11 +0000

    Sync LeetCode submission Runtime - 3 ms (87.52%), Memory - 43 MB (92.70%)

--- 0020-valid-parentheses/solution.java
+++ 0020-valid-parentheses/solution.java
@@ -1,23 +1,21 @@
 class Solution {
     public boolean isValid(String str) {
-        Stack<Character> stack=new Stack<Character>();
-        
-        for (char ch : str.toCharArray()) {
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stack.push(ch);
-            } 
-            else if (ch == ')' || ch == '}' || ch == ']') {
-                if (stack.isEmpty()) return false;
-                
-                char top = stack.pop();
-                if ((ch == ')' && top != '(') ||
-                    (ch == '}' && top != '{') ||
-                    (ch == ']' && top != '[')) {
+        Stack<Character> st=new Stack<>();
+        for(char ch:str.toCharArray()){
+            if(ch=='('||ch=='['||ch=='{'){
+                st.push(ch);
+            }
+            
+            if(ch==')'||ch=='}'||ch==']'){
+                if(st.isEmpty()) return false;
+                char top=st.pop();
+                if(ch==')'&&top!='(' ||
+                ch==']'&&top!='[' ||
+                ch=='}'&&top!='{'){
                     return false;
                 }
             }
-        }
-        
-        return stack.isEmpty();
+        } 
+        return st.isEmpty();
     }
 }
