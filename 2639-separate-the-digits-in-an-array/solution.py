2d26e2eba8f8a1e52714106dc2264c0c101a5de4
Subasree S <subasree3008@gmail.com>
2025-12-11 05:49:13 +0000

    Sync LeetCode submission Runtime - 4 ms (31.26%), Memory - 18 MB (100.00%)

---
+++ 2639-separate-the-digits-in-an-array/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def separateDigits(self, nums: List[int]) -> List[int]:
+        list1=[]
+        for i in nums:
+            for j in str(i):
+                list1.append(int(j))
+        return list1
+
