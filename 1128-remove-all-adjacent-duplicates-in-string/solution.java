f121e569e90aa1ac8eb3a056cc45024336c42ecd
Subasree S <subasree3008@gmail.com>
2025-12-16 06:48:02 +0000

    Sync LeetCode submission Runtime - 224 ms (5.58%), Memory - 49.8 MB (7.17%)

--- 1128-remove-all-adjacent-duplicates-in-string/solution.java
+++ 1128-remove-all-adjacent-duplicates-in-string/solution.java
@@ -1,18 +1,25 @@
 class Solution {
     public String removeDuplicates(String s) {
         Stack<Character> st=new Stack<>();
-        for(char ch:s.toCharArray()){
-            if(!st.isEmpty()&&st.peek()==ch){
-                st.pop();
+        char[] arr=s.toCharArray();
+        for(char i:arr){
+            if(!st.isEmpty() && i==st.peek()){
+                    st.pop();
             }
             else{
-                st.push(ch);
+                st.push(i);
             }
         }
-        StringBuilder sb=new StringBuilder();
-        for(char c:st){
-            sb.append(c);
+        String s1="";
+        while(!st.isEmpty()){
+            s1+=st.pop();
         }
-        return sb.toString();
+        System.out.print(s1);
+        char[] ch=s1.toCharArray();
+        String s2="";
+        for(int i=ch.length-1;i>=0;i--){
+            s2+=ch[i];
+        }
+        return s2;
     }
 }
