21a438e20895499e9ae907815c523e24d0b7b849
Subasree S <subasree3008@gmail.com>
2025-12-18 08:54:21 +0000

    Sync LeetCode submission Runtime - 350 ms (99.05%), Memory - 0.0B (100.00%)

--- 0182-duplicate-emails/solution.sql
+++ 0182-duplicate-emails/solution.sql
@@ -1,6 +1,3 @@
 # Write your MySQL query statement below
-SELECT email AS Email
-FROM Person
-GROUP BY email
-HAVING COUNT(*) > 1;
+select email as Email from person group by email having count(email)>1;
 
