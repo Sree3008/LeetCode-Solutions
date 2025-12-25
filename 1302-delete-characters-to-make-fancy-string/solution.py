0d8009fbd0a85a1bb260a9eba77f5e6ec29ee5f7
Subasree S <subasree3008@gmail.com>
2025-12-25 02:06:38 +0000

    Sync LeetCode submission Runtime - 299 ms (41.87%), Memory - 19.2 MB (100.00%)

---
+++ 1302-delete-characters-to-make-fancy-string/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        res=[]
+        for i in s:
+            if len(res)>=2 and i==res[-1] and i==res[-2] :
+                continue
+            res.append(i)
+        return ''.join(res) 
