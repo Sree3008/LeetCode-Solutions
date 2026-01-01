448f0e56937c707c3f3dc60b54dbc51cb1d42ebf
Subasree S <subasree3008@gmail.com>
2026-01-01 17:07:34 +0000

    Sync LeetCode submission Runtime - 8 ms (12.30%), Memory - 45.4 MB (40.53%)

--- 0137-single-number-ii/solution.java
+++ 0137-single-number-ii/solution.java
@@ -1,8 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
-     
+        
        Arrays.sort(nums);
-      // if(nums.length==1) return nums[0];
        for(int i=0;i<nums.length-2;i+=3){
         if(nums[i]!=nums[i+1]){
             return nums[i];
