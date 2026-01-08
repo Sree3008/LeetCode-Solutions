f2b82e0bd1368f29ea8d60a66c059dbc0dfb5fe7
Subasree S <subasree3008@gmail.com>
2026-01-08 11:55:15 +0000

    Sync LeetCode submission Runtime - 4 ms (39.49%), Memory - 19.4 MB (10.16%)

---
+++ 3997-maximize-sum-of-at-most-k-distinct-elements/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxKDistinct(self, nums: List[int], k: int) -> List[int]:
+       nums=list(set(nums))
+       nums.sort(reverse=True)
+       return nums[:k]
+                
+
+            
+        
