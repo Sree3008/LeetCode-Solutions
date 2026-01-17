38fe5596237f5b3fb3733a9f0cbd8d4f0c01d70f
Subasree S <subasree3008@gmail.com>
2026-01-17 14:51:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.5 MB (5.76%)

---
+++ 0147-insertion-sort-list/solution.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        list1=[]
+        temp=head
+        while temp:
+            list1.append(temp.val)
+            temp=temp.next
+        list1.sort()
+        head=None
+        temp=None
+        for i in list1:
+            newNode=ListNode(i)
+            if head is None:
+                head=newNode
+                temp=newNode
+            else:
+                temp.next=newNode 
+                temp=newNode
+        return head
+            
+
