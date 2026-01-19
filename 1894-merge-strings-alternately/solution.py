b70ffd398aa646402792cfea6a388c10c011c344
Subasree S <subasree3008@gmail.com>
2026-01-19 06:21:11 +0000

    Sync LeetCode submission Runtime - 59 ms (5.86%), Memory - 19.6 MB (17.30%)

--- 1894-merge-strings-alternately/solution.py
+++ 1894-merge-strings-alternately/solution.py
@@ -1,25 +1,24 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        s1=''
-        list1=list(word1)
-        list2=list(word2)
+        s=''
+        if len(word1)>len(word2):
+            for i in range(len(word2)):
+                s+=word1[i]
+                s+=word2[i]
+            else:
+                for i in range(len(word2),len(word1)):
+                    s+=word1[i]
         if len(word1)==len(word2):
+            for i in range(len(word2)):
+                s+=word1[i]
+                s+=word2[i]
+        if len(word2)>len(word1):
             for i in range(len(word1)):
-                s1+=list1[i]
-                s1+=list2[i]
-        elif len(word1)>len(word2):
-            for i in range(len(list1)):
-                if i<len(list2):
-                    s1+=list1[i]
-                    s1+=list2[i]
-                else:
-                    s1+=list1[i]
-        else:
-            for i in range(len(list2)):
-                if i<len(list1):
-                    s1+=list1[i]
-                    s1+=list2[i]
-                else:
-                    s1+=list2[i]
-        return s1
+                s+=word1[i]
+                s+=word2[i]
+            else:
+                for i in range(len(word1),len(word2)):
+                    s+=word2[i]
+        return s
+        
 
